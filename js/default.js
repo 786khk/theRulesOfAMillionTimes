cbc0df115355ca5336111cc06eb6bd25784f03b0
786khk <78067072+786khk@users.noreply.github.com>
2022-11-27 21:39:28 +0900

    init

---
+++ js/default.js
@@ -0,0 +1,6 @@
+const startBtn = document.getElementsByClassName('start_btn');
+
+shareBtn.addEventListener('click',)
+openBtn.addEventListener('click',)
+colorBtn.addEventListener('click',)
+startBtn.addEventListener('click',)
